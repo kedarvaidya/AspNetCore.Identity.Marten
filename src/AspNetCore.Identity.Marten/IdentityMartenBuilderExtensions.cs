04bf70d76968992f06ced807f4ffd972fcb65f77
Kedar <kedarvaidya0@gmail.com>
2016-08-29 21:55:27 +0530

    Add IdentityBuilder and StoreOptions extensions

---
+++ src/AspNetCore.Identity.Marten/IdentityMartenBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace AspNetCore.Identity.Marten
+{
+    public static class IdentityMartenBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a Marten implementation of identity information stores.
+        /// </summary>
+        /// <typeparam name="TUser"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="self">The <see cref="IdentityBuilder"/> instance this method extends.</param>
+        /// <returns>The <see cref="IdentityBuilder"/> instance this method extends.</returns>
+        public static IdentityBuilder AddMartenStores<TUser, TKey>(this IdentityBuilder self) where TUser: IdentityUser<TKey>
+        {
+            self.Services.TryAddScoped<IUserStore<TUser>, UserStore<TUser, TKey>>();
+
+            return self;
+        }
+    }
+}
---
+++ src/AspNetCore.Identity.Marten/IdentityStoreOptionsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Identity.Marten.Internal;
+using Marten;
+
+namespace AspNetCore.Identity.Marten
+{
+    public static class IdentityStoreOptionsExtensions
+    {
+        public static void ConfigureIdentityStoreOptions<TUser, TKey>(this StoreOptions self) where TUser: IdentityUser<TKey>
+        {
+            self.Linq.MethodCallParsers.Add(new HasStringifiedId<TKey>());
+
+            self.Schema.For<TUser>().Index(u => u.NormalizedUserName, i =>
+            {
+                i.IsUnique = true;
+            });
+
+            self.Schema.For<TUser>().Index(u => u.NormalizedEmail, i =>
+            {
+                i.IsUnique = true;
+            });
+        }
+    }
+}
